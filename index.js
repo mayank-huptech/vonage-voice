93eb516fcb7982fd1cd8a3637757482b4dc56171
Mayank23599Parmar <mayankparmar23599@gmail.com>
2024-02-13 18:59:53 +0530

    add sound.waw

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require('dotenv').config();
 const { Vonage }  = require('@vonage/server-sdk');
+const WaveFile = require('wavefile').WaveFile;
 const bodyParser = require('body-parser');
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { NCCOBuilder, Talk, OutboundCallWithNCCO } = require('@vonage/voice')
 const app = express();
+const expressWs = require('express-ws')(app);
 app.use(bodyParser.json());
 app.enable('trust proxy')
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+function chunkArray(array, chunkSize) {
+  var chunkedArray = [];
+  for (var i = 0; i < array.length; i += chunkSize)
+      chunkedArray.push(array.slice(i, i + chunkSize));
+  return chunkedArray;
+}
 // const privatePath=path.resolve(__dirname,process.env.VONAGE_PRIVATE_KEY_PATH)
 // var privateKey = fs.readFileSync(privatePath, 'utf8');
 // const credentials = {
@@ -35,26 +45,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   }
 //   makeCall();
 console.log("wdwddwdwdwdwd")
-app.get('/webhooks/events', (req, res) => {
+app.post('/webhooks/events', (req, res) => {
    console.log(req,"req.body.status")
-  res.status(200).end()
+   res.send(200);
 })
   app.get('/webhooks/answer', (req, res) => {
     // console.log(req.body,'req.body')
     // const number = req.body.from.split('').join(' ');
     console.log(req?.body,req?.params,req?.query)
-    const ncco = [
+    let nccoResponse = [
       {
-        action: 'talk',
-        text: 'Thank you for calling to mayank. mayank is available soon as possible',
-        language: "en-US"
+          "action": "talk",
+          "text": "Please wait while we connect you to the echo server"
       },
+      {
+          "action": "connect",
+          "from": "NexmoTest",
+          "endpoint": [
+              {
+                  "type": "websocket",
+                  "uri": `wss://${req.hostname}/socket`,
+                  "content-type": "audio/l16;rate=16000",
+              }
+          ]
+      }
+  ]
 
-    ];
     console.log("call comming,.....")
-    res.status(200).json(ncco);
+    res.status(200).json(nccoResponse);
   });  
+  expressWs.getWss().on('connection', function (ws) {
+    console.log('Websocket connection is open');
+  });
+  app.ws('/socket', (ws, req) => {
+    const wav = new WaveFile(fs.readFileSync("./sound.wav"));
+    wav.toSampleRate(16000);
+    wav.toBitDepth("16");
 
+    const samples = chunkArray(wav.getSamples()[0], 320);
+    for (var index = 0; index < samples.length; ++index) {
+        ws.send(Uint16Array.from(samples[index]).buffer);
+    }
+})
 const ANSWER_URL = 'https://raw.githubusercontent.com/nexmo-community/ncco-examples/gh-pages/text-to-speech.json'
 
 // vonage.voice.createOutboundCall({
