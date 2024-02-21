7f2e0bf32e9a70ff9d59e40a6851d62cb8339f9d
Mayank23599Parmar <mayankparmar23599@gmail.com>
2024-02-21 13:41:18 +0530

    vonage app proxy done

--- index.js
+++ index.js
@@ -1,85 +1,35 @@
 require('dotenv').config();
-const { Vonage }  = require('@vonage/server-sdk');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 app.use(bodyParser.json());
-app.enable('trust proxy')
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// const privatePath=path.resolve(__dirname,process.env.VONAGE_PRIVATE_KEY_PATH)
-// var privateKey = fs.readFileSync(privatePath, 'utf8');
-// const credentials = {
-//   apiKey: process.env.VONAGE_API_KEY,
-//   apiSecret: process.env.VONAGE_API_SECRET,
-//   applicationId: process.env.VONAGE_APPLICATION_ID,
-//   privateKey: privateKey
-// };
-// const options = {debug: true};
-// const vonage = new Vonage(credentials, options);
-
-// const ANSWER_URL = 'https://raw.githubusercontent.com/nexmo-community/ncco-examples/gh-pages/text-to-speech.json'
-
-
-console.log("wdwddwdwdwdwd")
 app.get('/webhooks/events', (req, res) => {
-   console.log(req.query,"/webhooks/events")
-   let nccoResponse = [
-    {
-        action: "talk",
-        text: "Please wait while we connect you to the echo server"
-    }
-]
-  console.log("/webhooks/events")
-  res.status(200).json(nccoResponse);
+  // console.log(req.query, "/webhooks/events")
+  res.status(200)
 })
 app.get('/webhooks/answer', (req, res) => {
-    console.log(req?.query,"/webhooks/answer")
-    const {to,from}=req?.query
-    console.log(to,from,"wddwd")
-    let nccoResponse = [
-      {
-          action: "talk",
-          text: "Hello mayank is bussy right now please wait for sometime. or mayank will get back later"
-      },
-      {
-        action: "connect",
-        timeout: 1000,
-        from: from,
-        endpoint: [
-          {
-            type: "phone",
-            number: 919726223558,
-          }
-        ]
-      }
-   
+  let nccoResponse = [
+    {
+      action: "talk",
+      text: "This call is being directed from Best Cataract Surgeons in America"
+    },
+    {
+      action: "connect",
+      from: process.env.VONAGE_NUMBER,
+      endpoint: [
+        {
+          type: "phone",
+          number: process.env.PROXY_NUMBER,
+        }
+      ]
+    }
   ]
-    console.log(" /webhooks/answer call comming,.....")
-    res.status(200).json(nccoResponse);
-  });  
+  res.status(200).json(nccoResponse)
+});
 
-  app.get("/webhooks/fallback",(req,res)=>{
-    console.log(req?.query,"/webhooks/fallback")
-    // [
-    //   {
-    //     action: "connect",
-    //     from: "447700900000",
-    //     timeout: 5,
-    //     eventType: "synchronous",
-    //     eventUrl: [
-    //      "https://example.com/event-fallback"
-    //     ],
-    //     endpoint: [
-    //       {
-    //         type: "phone",
-    //         number: "447700900001"
-    //       }
-    //     ]
-    //   }
-    // ]
-    
-  })
+app.get("/webhooks/fallback", (req, res) => {
+  // console.log(req?.query, "/webhooks/fallback")
+})
 
-  app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`));
